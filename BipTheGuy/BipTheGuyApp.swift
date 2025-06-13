41daa882bbbf7f1a1ef4ee7606275ab42b7a0f0c
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-13 17:32:03 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Tushar Munge on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
