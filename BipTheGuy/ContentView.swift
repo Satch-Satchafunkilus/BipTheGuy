9c7cc85a7fbde80c2b950780e8dc4b42f0c24994
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-13 19:10:30 -0500

    Completed BipTheGuy App - SwiftUI course by Prof. Gallaugher

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,17 +6,20 @@
 //
 
 import AVFAudio
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var isFullSize = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
 
     var body: some View {
         VStack {
             Spacer()
 
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(isFullSize ? 1.0 : 0.9)
@@ -37,14 +40,27 @@ struct ContentView: View {
 
             Spacer()
 
-            Button {
-                //TODO: Button Action here
-
-            } label: {
+            PhotosPicker(
+                selection: $selectedPhoto,
+                matching: .images,
+                preferredItemEncoding: .automatic
+            ) {
                 Label(
                     "Photo Library",
                     systemImage: "photo.fill.on.rectangle.fill"
                 )
+            }
+            .onChange(of: selectedPhoto) {
+                Task
+                {
+                    guard let selectedImage = try? await selectedPhoto?.loadTransferable(type: Image.self) else {
+                        print("😡 ERROR: Could not load selected photo")
+                        
+                        return
+                    }
+                    
+                    bipImage = selectedImage
+                }
             }
         }
         .padding()
